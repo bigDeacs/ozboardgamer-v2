c95f81a350113a5e2a0e12e4e6c73be1ed3b60e4
DESKTOP-OTGFGAB\brent <brentdeacon23@gmail.com>
2017-10-20 15:57:19 +1000

    Testing

---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test  */
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $this->be($user = factory('App\User')->create());
+
+        $thread = create('App\Thread');
+
+        $reply = make('App\Reply');
+
+        $this->post($thread->path().'/replies', $reply->toArray());
+
+        $this->get($thread->path())
+          ->assertSee($reply->body);
+    }
+
+    /** @test  */
+    function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $thread = create('App\Thread');
+
+        $reply = create('App\Reply');
+
+        $this->post('/threads/1/replies', []);
+    }
+}
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    /** @test  */
+    public function a_user_can_browse_threads()
+    {
+        $this->get('/threads')
+          ->assertSee($this->thread->title);
+    }
+
+    /** @test  */
+    function a_user_can_read_a_single_thread()
+    {
+        $this->get($this->thread->path())
+          ->assertSee($this->thread->title);
+    }
+
+    /** @test  */
+    function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
+
+        $this->get($this->thread->path())
+          ->assertSee($reply->body);
+    }
+}
